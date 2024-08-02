1c0ab4f5246241ac36db8a57b747208df03b01e4
Alex_Stanis <alexstanis11@gmail.com>
2024-08-02 14:40:53 +0300

    add data to db + create view for AP

--- drfsite/cars/admin.py
+++ drfsite/cars/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Car
 
-admin.site.register(Car)
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['brand', 'model', 'max_speed', 'power', 'cat']
+    list_editable = ['max_speed', 'power']
+    search_fields = ['brand', 'model']
--- drfsite/cars/models.py
+++ drfsite/cars/models.py
@@ -5,6 +5,8 @@ class Car(models.Model):
     brand = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
     content = models.TextField(blank=True)
+    power = models.CharField(max_length=20, null=True, blank=True)
+    max_speed = models.IntegerField(default=0)
     time_created = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
