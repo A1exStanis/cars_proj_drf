e619722d4fcfafa58eb8226d40ab5be0c11bc825
Alex_Stanis <alexstanis11@gmail.com>
2024-08-02 12:54:29 +0300

    create proj + models

---
+++ drfsite/cars/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Car(models.Model):
+    brand = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    content = models.TextField(blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+    cat = models.ForeignKey('Class', on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return f'{self.brand} {self.model}'
+
+
+class Class(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return self.name
