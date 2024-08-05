8669ef98dcbd0ffd551be04d3ad1de8b13ee06c4
Alex_Stanis <alexstanis11@gmail.com>
2024-08-05 15:32:56 +0300

    add UpdateAPIView and RetrieveUpdateDestroyAPIView

--- drfsite/cars/urls.py
+++ drfsite/cars/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CarAPIView, CarAPIList
+from .views import *
 
 urlpatterns = [
     path('car-list/', CarAPIList.as_view()),
-    path('car-list/<int:pk>', CarAPIList.as_view()),
+    path('car-list/<int:pk>', CarAPIUpdate.as_view()),
+    path('car-detail/<int:pk>', CarAPIDetailView.as_view()),
 ]
--- drfsite/cars/views.py
+++ drfsite/cars/views.py
@@ -10,41 +10,52 @@ class CarAPIList (generics.ListCreateAPIView):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
 
-class CarAPIView(APIView):
-    def get(self, request):
-        cars = Car.objects.all()
-        return Response({'posts': CarSerializer(cars, many=True).data})
 
-    def post(self, request):
-        serializer = CarSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({'post': serializer.data})
+class CarAPIUpdate(generics.UpdateAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
 
-    def put(self, request, *args, **kwargs):
-        pk = kwargs.get('pk', None)
-        if not pk:
-            return Response({'error': 'Method PUT not allowed'})
 
-        try:
-            instance = Car.objects.get(pk=pk)
-        except:
-            return Response({'error': 'Object does not exist'})
+class CarAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
 
-        serializer = CarSerializer(data=request.data, instance=instance)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({'put': serializer.data})
 
-    def delete(self, request, *args, **kwargs):
-        pk = kwargs.get('pk', None)
-        if not pk:
-            return Response({'error': 'Method DELETE not allowed'})
-
-        try:
-            instance = Car.objects.get(pk=pk)
-        except:
-            return Response({'error': 'Object does not exist'})
-
-        instance.delete()
-        return Response({'delete': 'Object has been deleted'})
+# class CarAPIView(APIView):
+#     def get(self, request):
+#         cars = Car.objects.all()
+#         return Response({'posts': CarSerializer(cars, many=True).data})
+#
+#     def post(self, request):
+#         serializer = CarSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response({'post': serializer.data})
+#
+#     def put(self, request, *args, **kwargs):
+#         pk = kwargs.get('pk', None)
+#         if not pk:
+#             return Response({'error': 'Method PUT not allowed'})
+#
+#         try:
+#             instance = Car.objects.get(pk=pk)
+#         except:
+#             return Response({'error': 'Object does not exist'})
+#
+#         serializer = CarSerializer(data=request.data, instance=instance)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response({'put': serializer.data})
+#
+#     def delete(self, request, *args, **kwargs):
+#         pk = kwargs.get('pk', None)
+#         if not pk:
+#             return Response({'error': 'Method DELETE not allowed'})
+#
+#         try:
+#             instance = Car.objects.get(pk=pk)
+#         except:
+#             return Response({'error': 'Object does not exist'})
+#
+#         instance.delete()
+#         return Response({'delete': 'Object has been deleted'})
