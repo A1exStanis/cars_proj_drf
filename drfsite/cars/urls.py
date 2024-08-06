9d07ad57bc65f82ca93946a255299f28dea6529b
Alex_Stanis <alexstanis11@gmail.com>
2024-08-06 22:20:57 +0300

    use Viewset + routers

--- drfsite/cars/urls.py
+++ drfsite/cars/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(r'cars', CarViewSet)
 
 urlpatterns = [
-    path('car-list/', CarAPIList.as_view()),
-    path('car-list/<int:pk>', CarAPIUpdate.as_view()),
-    path('car-detail/<int:pk>', CarAPIDetailView.as_view()),
+    path('', include(router.urls)),
 ]
--- drfsite/cars/views.py
+++ drfsite/cars/views.py
@@ -1,61 +1,25 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Car
 from .serializers import CarSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
-class CarAPIList (generics.ListCreateAPIView):
+class CarViewSet(viewsets.ModelViewSet):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
 
-
-class CarAPIUpdate(generics.UpdateAPIView):
-    queryset = Car.objects.all()
-    serializer_class = CarSerializer
-
-
-class CarAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Car.objects.all()
-    serializer_class = CarSerializer
-
-
-# class CarAPIView(APIView):
-#     def get(self, request):
-#         cars = Car.objects.all()
-#         return Response({'posts': CarSerializer(cars, many=True).data})
+# class CarAPIList (generics.ListCreateAPIView):
+#     queryset = Car.objects.all()
+#     serializer_class = CarSerializer
 #
-#     def post(self, request):
-#         serializer = CarSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response({'post': serializer.data})
 #
-#     def put(self, request, *args, **kwargs):
-#         pk = kwargs.get('pk', None)
-#         if not pk:
-#             return Response({'error': 'Method PUT not allowed'})
+# class CarAPIUpdate(generics.UpdateAPIView):
+#     queryset = Car.objects.all()
+#     serializer_class = CarSerializer
 #
-#         try:
-#             instance = Car.objects.get(pk=pk)
-#         except:
-#             return Response({'error': 'Object does not exist'})
 #
-#         serializer = CarSerializer(data=request.data, instance=instance)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response({'put': serializer.data})
-#
-#     def delete(self, request, *args, **kwargs):
-#         pk = kwargs.get('pk', None)
-#         if not pk:
-#             return Response({'error': 'Method DELETE not allowed'})
-#
-#         try:
-#             instance = Car.objects.get(pk=pk)
-#         except:
-#             return Response({'error': 'Object does not exist'})
-#
-#         instance.delete()
-#         return Response({'delete': 'Object has been deleted'})
+# class CarAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Car.objects.all()
+#     serializer_class = CarSerializer
